a7fd090e531daea71e23802e2a483b5ad8683a92
partha1405 <roypartha0514@gmail.com>
2020-12-30 16:30:47 +0530

    Clean branch Project_Salary_Management

--- Nvsp/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("HostName","localhost");
-define("UserName","root");
-define("Password","");
-define("DbName","userdb");
-$con=mysqli_connect(HostName,UserName,Password) or die("Failed to connect to MySQL: "); 
-mysqli_select_db($con,DbName) or die("Failed to connect to database: ");
-?> 
